2112b152d634df154eb814c722a3ef9a120b8788
renmurakami <autoeyy@gmail.com>
2022-06-15 12:54:02 +0900

    tab nav

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,26 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "@shopify/restyle";
 import { StatusBar } from "expo-status-bar";
 import { VFC } from "react";
 import { View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import theme from "./theme/theme";
+import { RootTabParamList } from "./types/navigation/navigationType";
 import HomeView from "./views/homeView";
+
+const Tab = createBottomTabNavigator<RootTabParamList>();
 
 const Index: VFC = () => {
     return (
         <ThemeProvider theme={theme}>
-            <SafeAreaProvider>
-                <View style={{ flex: 1 }}>
-                    <HomeView />
-                    <StatusBar style="auto" />
-                </View>
-            </SafeAreaProvider>
+            <NavigationContainer>
+                <Tab.Navigator>
+                    <Tab.Screen name="Home" component={HomeView} />
+                    <Tab.Screen name="Setting" component={HomeView} />
+                </Tab.Navigator>
+            </NavigationContainer>
+            <StatusBar style="auto" />
         </ThemeProvider>
     );
 };
