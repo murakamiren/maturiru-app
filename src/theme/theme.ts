c6b8515afc91e6f4f11b2a1f355f3aa74d041837
renmurakami <autoeyy@gmail.com>
2022-06-15 11:50:59 +0900

    create theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,24 @@
+import { createTheme } from "@shopify/restyle";
+import { palette } from "./palette";
+
+const theme = createTheme({
+    colors: {
+        primary: palette.primaryColor,
+        secondary: palette.secondaryColor,
+        white: palette.white,
+        black: palette.black
+    },
+    spacing: {
+        s: 8,
+        m: 16,
+        l: 24,
+        xl: 40
+    },
+    breakpoints: {
+        phone: 0,
+        tablet: 768
+    }
+});
+
+export type Theme = typeof theme;
+export default theme;
