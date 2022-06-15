68a640191fb64be3b5e56f47de9b53cadeefa4c2
renmurakami <autoeyy@gmail.com>
2022-06-15 12:57:26 +0900

    remove unused import

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "@shopify/restyle";
 import { StatusBar } from "expo-status-bar";
 import { VFC } from "react";
-import { View } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 import theme from "./theme/theme";
 import { RootTabParamList } from "./types/navigation/navigationType";
 import HomeView from "./views/homeView";
