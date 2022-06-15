dcfb0b946632cada64b64864f11977ca7f3b58ec
renmurakami <autoeyy@gmail.com>
2022-06-15 14:33:58 +0900

    eslint import config

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,12 +19,32 @@ module.exports = {
         },
         project: "./tsconfig.json"
     },
-    plugins: ["react", "react-hooks", "@typescript-eslint"],
+    plugins: ["react", "react-hooks", "@typescript-eslint", "import"],
     rules: {
         indent: ["error", 4],
         "linebreak-style": ["error", "unix"],
         quotes: ["error", "double"],
         semi: ["error", "always"],
-        "react/react-in-jsx-scope": "off"
+        "react/react-in-jsx-scope": "off",
+        "import/order": [
+            "error",
+            {
+                groups: [
+                    "builtin", // 組み込みモジュール
+                    "external", // npmでインストールした外部ライブラリ
+                    "internal", // 自作モジュール
+                    ["parent", "sibling"],
+                    "object",
+                    "type",
+                    "index"
+                ],
+                "newlines-between": "always",
+                pathGroupsExcludedImportTypes: ["builtin"],
+                alphabetize: {
+                    order: "asc", // 昇順にソート
+                    caseInsensitive: true // 小文字大文字を区別する
+                }
+            }
+        ]
     }
 };
