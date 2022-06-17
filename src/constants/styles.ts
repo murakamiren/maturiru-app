4f36e768830f37ed6718cb9b851efdc478ef5ee2
renmurakami <autoeyy@gmail.com>
2022-06-17 11:53:09 +0900

    themeColor, navigationIcon, add react native svg transformer metro config

---
+++ src/constants/styles.ts
@@ -0,0 +1,8 @@
+export const tabBarIconSize = 20;
+
+export const themeColors = {
+    primaryColor: "#F13A00",
+    secondaryColor: "#0B135C",
+    black: "#0B0B0B",
+    white: "#FBFBFB"
+};
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { VFC } from "react";
 
+import SearchIcon from "./assets/svg/searchIcon.svg";
+import { tabBarIconSize, themeColors } from "./constants/styles";
 import { RootTabParamList } from "./types/navigation/navigationType";
 import HomeView from "./views/homeView";
 import TestView from "./views/testView";
@@ -13,7 +15,23 @@ const Index: VFC = () => {
     return (
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeView} />
+                <Tab.Screen
+                    name="Home"
+                    component={HomeView}
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <SearchIcon
+                                width={tabBarIconSize}
+                                height={tabBarIconSize}
+                                stroke={
+                                    focused
+                                        ? themeColors.primaryColor
+                                        : themeColors.black
+                                }
+                            />
+                        )
+                    }}
+                />
                 <Tab.Screen name="Setting" component={TestView} />
             </Tab.Navigator>
             <StatusBar style="auto" />
