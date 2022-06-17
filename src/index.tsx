bb586670ee369550260c613346542cdcd93ba068
renmurakami <autoeyy@gmail.com>
2022-06-17 12:02:58 +0900

    tabbar icon test, text

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { VFC } from "react";
+import { StyleSheet, Text } from "react-native";
 
 import SearchIcon from "./assets/svg/searchIcon.svg";
+import TimelineIcon from "./assets/svg/timelineIcon.svg";
 import { tabBarIconSize, themeColors } from "./constants/styles";
 import { RootTabParamList } from "./types/navigation/navigationType";
 import HomeView from "./views/homeView";
@@ -19,6 +21,10 @@ const Index: VFC = () => {
                     name="Home"
                     component={HomeView}
                     options={{
+                        title: "検索",
+                        tabBarLabel: () => (
+                            <Text style={styles.navText}>検索</Text>
+                        ),
                         tabBarIcon: ({ focused }) => (
                             <SearchIcon
                                 width={tabBarIconSize}
@@ -32,11 +38,38 @@ const Index: VFC = () => {
                         )
                     }}
                 />
-                <Tab.Screen name="Setting" component={TestView} />
+                <Tab.Screen
+                    name="Setting"
+                    component={TestView}
+                    options={{
+                        title: "タイムライン",
+                        tabBarLabel: () => (
+                            <Text style={styles.navText}>タイムライン</Text>
+                        ),
+                        tabBarIcon: ({ focused }) => (
+                            <TimelineIcon
+                                width={tabBarIconSize}
+                                height={tabBarIconSize}
+                                stroke={
+                                    focused
+                                        ? themeColors.primaryColor
+                                        : themeColors.black
+                                }
+                            />
+                        )
+                    }}
+                />
             </Tab.Navigator>
             <StatusBar style="auto" />
         </NavigationContainer>
     );
 };
+
+const styles = StyleSheet.create({
+    navText: {
+        fontSize: 8,
+        color: themeColors.black
+    }
+});
 
 export default Index;
