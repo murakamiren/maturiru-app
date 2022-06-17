d2817813c1a875bdf7f3426a99863bd9607d1213
renmurakami <autoeyy@gmail.com>
2022-06-17 20:33:54 +0900

    remove unused type

--- src/types/componentsProps/navigationTabScreenProps.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentType, FC } from "react";
-import { SvgProps } from "react-native-svg";
-
-export type NavigationTabScreenProps = {
-    title: string;
-    label: string;
-    component: ComponentType<any>;
-    Icon: FC<SvgProps>;
-};
