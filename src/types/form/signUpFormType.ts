f1c2bfc95b64f3dea4fb4abacca5c46d8a53020a
renmurakami <autoeyy@gmail.com>
2022-07-02 11:01:19 +0900

    add reach hook form

---
+++ src/types/form/signUpFormType.ts
@@ -0,0 +1,10 @@
+export type SignUpFormInputType = {
+    lastNameJa: string;
+    firstNameJa: string;
+    lastNameRoman: string;
+    firstNameRoman: string;
+    age: number;
+    gender: "man" | "woman" | "else";
+    email: string;
+    phoneNumber: number;
+};
--- src/views/login/signupView.tsx
+++ src/views/login/signupView.tsx
@@ -1,12 +1,24 @@
 import { VFC } from "react";
-import { Text, View } from "react-native";
+import { Controller, useForm } from "react-hook-form";
+import { TextInput, View } from "react-native";
 
 import { themeColors } from "../../constants/styles";
+import { SignUpFormInputType } from "../../types/form/signUpFormType";
 
 const SignUpView: VFC = () => {
+    const {
+        control,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<SignUpFormInputType>();
     return (
         <View style={{ flex: 1, backgroundColor: themeColors.white }}>
-            <Text>aaaaa</Text>
+            <Controller
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value } }) => <TextInput />}
+                name="lastNameJa"
+            />
         </View>
     );
 };
